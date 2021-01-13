ebf1ca52873a94bbc205c5bfb4e640fae9dc0b54
T_Tanya <tikhonovskyat@gmail.com>
2021-01-13 22:45:51 +0300

    Updates

---
+++ precache-manifest.2c2bbfbc099f78a1531891387b6ddde2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436424032330577121875f71d5b878c4",
+    "url": "/sport_blog/index.html"
+  },
+  {
+    "revision": "dee74a836035bca26304",
+    "url": "/sport_blog/static/css/main.aba3fe92.chunk.css"
+  },
+  {
+    "revision": "b2b15e35ff0c70d1b36c",
+    "url": "/sport_blog/static/js/2.41ae1098.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/sport_blog/static/js/2.41ae1098.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dee74a836035bca26304",
+    "url": "/sport_blog/static/js/main.198c4eb1.chunk.js"
+  },
+  {
+    "revision": "dec49cae2f86ecc65476",
+    "url": "/sport_blog/static/js/runtime-main.54df94f7.js"
+  },
+  {
+    "revision": "c4baa750eccfb0f2d51ac2b63aba64b4",
+    "url": "/sport_blog/static/media/131158382_2592563177702165_5456903758542119598_n-e1608125189377.c4baa750.jpg"
+  },
+  {
+    "revision": "f2cfade6b0b571061788a8610c7d24bf",
+    "url": "/sport_blog/static/media/3-1-800x445.f2cfade6.jpg"
+  },
+  {
+    "revision": "8332d8a8f7e2dc70b8a24ceec14bfd0e",
+    "url": "/sport_blog/static/media/baydarka-9-1024x683.8332d8a8.jpg"
+  },
+  {
+    "revision": "b4c9a6de9cf3736e8cbb1e1004efa40c",
+    "url": "/sport_blog/static/media/bczp_kiev-e1610197555770.b4c9a6de.jpg"
+  },
+  {
+    "revision": "03c55cb09ceb0df2bf516e269ea2259a",
+    "url": "/sport_blog/static/media/cher2.03c55cb0.jpg"
+  },
+  {
+    "revision": "78710204af14e65d60cb597a8cba4083",
+    "url": "/sport_blog/static/media/kramarenko.78710204.jpg"
+  },
+  {
+    "revision": "4dc37c441a8728e8b43fc9db60b799e4",
+    "url": "/sport_blog/static/media/kuzko_baza.4dc37c44.jpg"
+  },
+  {
+    "revision": "49069219a4a67d6c1cc47af0a37b03a2",
+    "url": "/sport_blog/static/media/orig_421395.49069219.jpg"
+  },
+  {
+    "revision": "6204a337101f29a2b14dffd576fc32b3",
+    "url": "/sport_blog/static/media/seamless-sports-pattern-background_98292-4294.6204a337.jpg"
+  }
+]);
